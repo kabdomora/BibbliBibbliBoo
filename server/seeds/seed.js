46758bbd29663814d266bffb6f761019fe2faba2
kabdomora <114969831+kabdomora@users.noreply.github.com>
2023-03-19 16:41:22 -0700

    user seed starter

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -2,12 +2,15 @@ const db = require('../config/connection');
 const { Book } = require('../models');
 
 const bookData = require('./bookData.json');
+const userData = require('./userData.json');
 
 db.once('open', async () => {
   await Book.deleteMany({});
+  await User.deleteMany({});
 
   const books = await Book.insertMany(bookData);
+  const users = await User.insertMany(userData);
 
-  console.log('Books seeded! 📚');
+  console.log('Books and Users seeded! 💃🕺📚');
   process.exit(0);
 });
